c7c51494583759afd0fce3e205b03b7e57d69009
Matthieu Petelot <matthieu.petelot5@etu.univ-lorraine.fr>
2025-04-01 18:12:55 +0200

    [Modify] rajout des derniers tests de TelecomTest

--- Eclipse Domotique/test/TelecommandeTest.java
+++ Eclipse Domotique/test/TelecommandeTest.java
@@ -32,10 +32,56 @@ class TelecommandeTest {
         t.ajouterLampe(l2);
         //verification
         assertFalse(t.getLampes()[1].isAllume());
-        assertTrue(t.getLampes()[1].getNom()==l2.getNom());
-
+        assertSame(t.getLampes()[1].getNom(), l2.getNom());
     }
 
+    /**
+     * methode permettant de tester l'activation d'une lampe en position 0
+     */
+    @Test
+    void testActivation_0() {
+        //prepa donnée
+        Telecommande t = new Telecommande();
+        Lampe l = new Lampe("lampeTest1");
+        t.ajouterLampe(l);
+        //methode testée
+        t.getLampes()[0].allumer();
+        //verifications
+        assertTrue(t.getLampes()[0].isAllume());
+    }
 
+    /**
+     * methode permettant de tester l'activation d'une lampe en position 1
+     */
+    @Test
+    void testActivation_1() {
+        //prepa donnée
+        Telecommande t = new Telecommande();
+        Lampe l = new Lampe("lampeTest1");
+        Lampe l2 = new Lampe("lampeTest2");
+        t.ajouterLampe(l);
+        t.ajouterLampe(l2);
+        //methode testée
+        t.getLampes()[0].allumer();
+        //verifications
+        assertTrue(t.getLampes()[1].isAllume());
+    }
 
+    /**
+     * methode permettant de tester l'activation d'une lampe inexistante
+     */
+    @Test
+    void testActivation_null() {
+        boolean erreurLevee = false;
+        try {
+            //prepa données
+            Telecommande t = new Telecommande();
+            //methode testée
+            t.getLampes()[0].allumer();
+        } catch (NullPointerException e) {
+            erreurLevee = true;
+        }
+        assertTrue(erreurLevee);
+    }
+    
 }
