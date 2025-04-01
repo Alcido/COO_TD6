45ab2fea3ce901770d0e35dabcbf18733b862d01
GPOIRE <gpoire.developpement@gmail.com>
2025-04-01 17:30:52 +0200

    [Create] Création de la base des méthodes de Telecommande

---
+++ Eclipse Domotique/src/Telecommande.java
@@ -0,0 +1,27 @@
+public class Telecommande {
+
+    private Lampe[] lampes;
+
+    public Telecommande(){
+        lampes = new Lampe[100];
+    }
+    public void activerLampe(){
+        throw new Error("Code à faire");
+    }
+
+    public void ajouterLampe(){
+        throw new Error("Code à faire");
+    }
+
+    public void desactiverLampe(){
+        throw new Error("Code à faire");
+    }
+
+    public void activerTout(){
+        throw new Error("Code à faire");
+    }
+
+    public String toString() {
+        throw new Error("Code à faire");
+    }
+}
