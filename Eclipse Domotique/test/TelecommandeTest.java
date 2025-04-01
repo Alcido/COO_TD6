9feb8f9e4a79c0e8796250e8176ed2a5e7ede2d1
Matthieu Petelot <matthieu.petelot5@etu.univ-lorraine.fr>
2025-04-01 18:01:18 +0200

    [Modify] creation des premiers tests

--- Eclipse Domotique/test/TelecommandeTest.java
+++ Eclipse Domotique/test/TelecommandeTest.java
@@ -15,8 +15,27 @@ class TelecommandeTest {
         //methode testée
         t.ajouterLampe(l);
         //verifications
-        //
+        assertFalse(t.getLampes()[0].isAllume());
+    }
+
+    /**
+     * Methode permettant de tester l'ajout dans une telecommande à 1 element
+     */
+    @Test
+    void testAjout_Lampe() {
+        //prepa des données
+        Telecommande t = new Telecommande();
+        Lampe l = new Lampe("lampeTest1");
+        Lampe l2 = new Lampe("lampeTest2");
+        //appel methode
+        t.ajouterLampe(l);
+        t.ajouterLampe(l2);
+        //verification
+        assertFalse(t.getLampes()[1].isAllume());
+        assertTrue(t.getLampes()[1].getNom()==l2.getNom());
 
     }
+
+
 
 }
